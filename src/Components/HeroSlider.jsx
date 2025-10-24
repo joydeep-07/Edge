7430bbf238e3e83d4442c7627b17aa31c4705b99
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-10-24 21:31:08 +0530

    Updated

--- src/Components/HeroSlider.jsx
+++ src/Components/HeroSlider.jsx
@@ -13,29 +13,30 @@ import {
 const HeroSlider = () => {
   return (
     <div>
-      <div className="mt-10 w-full border-t border-b border-gray-800 flex justify-center overflow-hidden bg-black py-3 relative">
-        {/* Fade effect containers */}
-        <div className="absolute left-0 top-0 bottom-0 w-100 bg-gradient-to-r from-black to-transparent z-10" />
-        <div className="absolute right-0 top-0 bottom-0 w-100 bg-gradient-to-l from-black to-transparent z-10" />
+      <div className="mt-10 w-full border-t border-b pb-3 border-gray-800 flex justify-center overflow-hidden bg-black py-2 relative">
+        {/* Fade edges */}
+        <div className="absolute left-0 top-0 bottom-0 w-20 bg-gradient-to-r from-black to-transparent z-10" />
+        <div className="absolute right-0 top-0 bottom-0 w-20 bg-gradient-to-l from-black to-transparent z-10" />
 
         <ScrollVelocity
           texts={[
             "Animation Community Development Websites Designing Graphics",
           ]}
-          velocity={80} 
+          velocity={80}
           parallaxClassName="w-full overflow-hidden"
           scrollerClassName="
-      text-sm md:text-base  
+      custom-scroll-text 
+      text-xs md:text-sm 
       font-medium 
       font-mono 
       text-gray-500 
-      tracking-wide 
+      tracking-wider 
       whitespace-nowrap
       hover:text-gray-400 
       transition-colors 
       duration-500
     "
-          numCopies={16}
+          numCopies={18}
           stiffness={180}
         />
       </div>
