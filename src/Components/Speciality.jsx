974b8f4da743abcd48431b6381c8769e416ccf3e
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-10-25 18:33:12 +0530

    Border removed

--- src/Components/Speciality.jsx
+++ src/Components/Speciality.jsx
@@ -140,28 +140,25 @@ const Speciality = () => {
         <ScrollVelocity
           texts={[
             <span className="flex items-center gap-4 whitespace-nowrap">
-              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 border border-gray-600 rounded-full text-sm">
+              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 bg-gray-500/20 rounded-full text-sm">
                 <SiMongodb color="#4DB33D" size={15} /> Mongo
               </span>
-              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 border border-gray-600 rounded-full text-sm">
-                <SiMongodb color="#4DB33D" size={15} /> DB
-              </span>
-              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 border border-gray-600 rounded-full text-sm">
+              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 bg-gray-500/20 rounded-full text-sm">
                 <SiExpress color="yellow" size={15} /> Express
               </span>
-              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 border border-gray-600 rounded-full text-sm">
+              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 bg-gray-500/20 rounded-full text-sm">
                 <SiReact color="#61DBFB" size={15} /> React
               </span>
-              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 border border-gray-600 rounded-full text-sm">
+              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 bg-gray-500/20 rounded-full text-sm">
                 <SiNodedotjs color="#8CC84B" size={15} /> Node
               </span>
-              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 border border-gray-600 rounded-full text-sm">
+              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 bg-gray-500/20 rounded-full text-sm">
                 <SiHtml5 color="#E34F26" size={15} /> HTML
               </span>
-              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 border border-gray-600 rounded-full text-sm">
+              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 bg-gray-500/20 rounded-full text-sm">
                 <SiCss3 color="#1572B6" size={15} /> CSS
               </span>
-              <span className="text-gray-400 font-medium flex items-center gap-1 px-4 py-2 border border-gray-600 rounded-full text-sm">
+              <span className="text-gray-400 font-medium m-2 flex items-center gap-1 px-4 py-2 bg-gray-500/20 rounded-full text-sm">
                 <SiTailwindcss color="#38BDF8" size={15} /> Tailwind
               </span>
             </span>,
