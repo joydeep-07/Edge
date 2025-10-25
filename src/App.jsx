51e0e00f1041f7db3e7219464f07b55dcc5365a9
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-10-25 21:43:49 +0530

    Lenis Applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import Home from './Pages/Home'
+import React, { useEffect } from "react";
+import Home from "./Pages/Home";
+import Lenis from "@studio-freight/lenis";
 
 const App = () => {
-  return (
-    <div>
-      <Home/>
-    </div>
-  )
-}
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2, 
+      smooth: true,
+      direction: "vertical",
+      gestureDirection: "vertical",
+      smoothTouch: false,
+      touchMultiplier: 1.5,
+    });
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
 
-export default App
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return (
+    <div className="bg-black text-white">
+      <Home />
+    </div>
+  );
+};
+
+export default App;
